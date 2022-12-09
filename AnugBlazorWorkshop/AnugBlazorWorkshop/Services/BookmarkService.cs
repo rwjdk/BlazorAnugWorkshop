fb305d86b827a3b362e5bb4596cad33dc7d98bc1
Rasmus Wulff Jensen <rasmus.wulff.jensen@outlook.com>
2022-12-09 11:41:55 +0100

    Bookmark list Done

---
+++ AnugBlazorWorkshop/AnugBlazorWorkshop/Services/BookmarkService.cs
@@ -0,0 +1,17 @@
+﻿using SharedModels.Model;
+
+namespace AnugBlazorWorkshop.Services;
+
+public class BookmarkService
+{
+    public async Task<List<Bookmark>> GetBookmarks()
+    {
+        await Task.CompletedTask; //to get rid of warning for now
+        return new List<Bookmark>()
+        {
+            new Bookmark() { Guid = "3a1e18db-d585-41ec-ad7c-3ba0088575c6", Title = "Google", Url = "https://www.google.com", Description = "Search Engine" },
+            new Bookmark() { Guid = "58ee60eb-e67a-4962-afd3-36d05121e607", Title = "Microsoft", Url = "https://www.microsoft.com", Description = "The people that made it possible not to write Javascript!" },
+            new Bookmark() { Guid = "b5684299-4da1-46ae-8f29-d94fa1265cbc", Title = "ANUG", Url = "http://www.anug.dk", Description = "Cool .NET UserGroup" }
+        };
+    }
+}
---
+++ AnugBlazorWorkshop/Shared/Model/Bookmark.cs
@@ -0,0 +1,10 @@
+﻿namespace SharedModels.Model
+{
+    public class Bookmark
+    {
+        public string Guid { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+    }
+}
